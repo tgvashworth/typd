bc47897ca8fe5027d4a153c18b364695af6bdca1
Tom Ashworth <tashworth@twitter.com>
2015-10-15 10:48:13 +0100

    feat: minfy distributed output

--- config/webpack.config.publish.js
+++ config/webpack.config.publish.js
@@ -1,6 +1,9 @@
+var webpack = require('webpack');
 var assign = require('object-assign');
 var constants = require('./constants');
 var baseConfig = require('./webpack.config');
+
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = assign(baseConfig, {
   output: {
@@ -8,5 +11,15 @@ module.exports = assign(baseConfig, {
     library: 'Typd',
     libraryTarget: 'commonjs2',
     path: constants.BUILD_DIRECTORY
-  }
+  },
+  plugins: baseConfig.plugins.concat([
+    new UglifyJsPlugin({
+      compress: {
+        dead_code: true,
+        drop_console: true,
+        screw_ie8: true,
+        warnings: true
+      }
+    })
+  ])
 });
